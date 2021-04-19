9a0993103ed25dcae4b7f650e3b9b8230ba35d44
abubaker <mohammad_abubaker@yahoo.com>
2021-04-19 18:08:17 +0500

    Working...

--- app/src/main/java/org/abubaker/permissionsexample/MainActivity.kt
+++ app/src/main/java/org/abubaker/permissionsexample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import org.abubaker.permissionsexample.databinding.ActivityMainBinding
@@ -31,16 +32,38 @@ class MainActivity : AppCompatActivity() {
         binding.btnCameraPermission.setOnClickListener {
 
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+                // Inform user that he has already access to the camera permission.
                 Toast.makeText(this, "You already have access to the permission", Toast.LENGTH_LONG).show()
+
             } else {
 
-                // Request Permission
-                Toast.makeText(this, "You do not have access to the permission", Toast.LENGTH_SHORT).show()
+                // Ask user to allow permission for the requested feature.
+                ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
 
             }
 
         }
 
+
+    }
+
+    /**
+     * Actions to take if Permission is allowed or denied.
+     */
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == CAMERA_PERMISSION_CODE) {
+
+            // Conditional action
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted for the Camera.", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "Oops you just denied the permission for camera. You can also allow it from the settings.", Toast.LENGTH_SHORT).show()
+            }
+
+        }
 
     }
 
